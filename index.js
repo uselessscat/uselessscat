1bf4f3cd7a7ab507fe79fe536aa02d17e8069749
Ariel Carvajal <arie.cbpro@gmail.com>
2020-09-06 20:17:09 -0300

    Generate automatically the readme

--- index.js
+++ index.js
@@ -1,48 +1,105 @@
-require('dotenv').config()
-
+const FileSystem = require('fs').promises;
+const DotEnv = require('dotenv')
+const Async = require('async');
+const Handlebars = require("handlebars");
 const { Octokit } = require('@octokit/core');
 
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+const bagdes = require('./data/bagdes.json');
 
-async function getRepositories() {
-    const response = await octokit.request('GET /user/repos', {
-        type: 'all',
+async function getRepository(octokit, path) {
+    [owner, repo] = path.split('/');
+
+    const response = octokit.request('GET /repos/{owner}/{repo}/topics', {
+        owner,
+        repo,
+        // preview feature
         mediaType: {
-            previews: ['mercy'], // get topics
-        },
-    });
-
-    octokit.request('GET /user/repos', {
-        type: 'all',
-        visibility: 'all',
-
+            previews: [
+                'mercy'
+            ]
+        }
     });
 
     return response;
 }
 
-function summarizeResults(response) {
-    const results = {
-        visibility: {
-            public: 0,
-            private: 0
-        },
-        
-    };
-
-    response.data.forEach(element => {
-
+async function getRepositories(octokit) {
+    const response = await octokit.request('GET /user/repos', {
+        type: 'all',
+        per_page: '100'
     });
 
-    return results;
+    const paths = response.data.map(repository => repository.full_name);
+    const repositories = Async.mapLimit(paths, 100, async path => getRepository(octokit, path));
+
+    return repositories;
+}
+
+function summarizeResults(response) {
+    const summarizations = response.reduce(
+        (previous, { data: { names: names } }) => {
+            names.push('git')
+
+            names.forEach(el => {
+                if (el in previous) {
+                    previous[el] += 1;
+                } else {
+                    previous[el] = 1;
+                }
+            });
+
+            return previous;
+        }, {});
+
+    return summarizations;
+}
+
+function fillRepositoryCount(original, data) {
+    original.sections.forEach(section => {
+        const topics = section.elements;
+
+        Object.keys(topics).forEach(key => {
+            const topic = topics[key].topic;
+
+            if (topic && topic in data) {
+                topics[key].message = `${data[key]}%20Repos`;
+            }
+        })
+    });
+
+    return original;
+}
+
+async function loadFile(filePath) {
+    const content = await FileSystem.readFile(filePath);
+
+    return content.toString();
+}
+
+async function writeFile(filePath, data) {
+    return FileSystem.writeFile(filePath, data)
+}
+
+function generateMarkdown(data, template) {
+    const markdown = Handlebars.compile(template)(data);
+
+    return markdown;
 }
 
 async function main() {
-    // TODO: iterate over pages
-    const response = getRepositories()
+    // load environment
+    DotEnv.config();
 
-    const summarized = summarizeResults(await response)
-    console.log(summarized)
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+    // TODO: iterate over pages
+    const response = getRepositories(octokit);
+    const template = loadFile('./templates/readme.md.handlebars')
+
+    const summarized = fillRepositoryCount(bagdes, summarizeResults(await response));
+    const markdown = generateMarkdown(summarized, await template);
+
+    await writeFile('readme.md', markdown);
 }
 
 main()
