6b3ac56255f2933c5af6f1c3ef3b104bf8d4170c
Ariel Carvajal <arie.cbpro@gmail.com>
2021-07-02 22:20:31 -0400

    Refactoring

--- index.js
+++ index.js
@@ -1,75 +1,10 @@
 const FileSystem = require('fs').promises;
-const DotEnv = require('dotenv')
+const DotEnv = require('dotenv');
 const Async = require('async');
-const Handlebars = require("handlebars");
-const { Octokit } = require('@octokit/core');
+const Handlebars = require('handlebars');
+const { Octokit } = require('octokit');
 
 const bagdes = require('./data/bagdes.json');
-
-async function getRepository(octokit, path) {
-    [owner, repo] = path.split('/');
-
-    const response = octokit.request('GET /repos/{owner}/{repo}/topics', {
-        owner,
-        repo,
-        // preview feature
-        mediaType: {
-            previews: [
-                'mercy'
-            ]
-        }
-    });
-
-    return response;
-}
-
-async function getRepositories(octokit) {
-    const response = await octokit.request('GET /user/repos', {
-        type: 'all',
-        per_page: '100'
-    });
-
-    const paths = response.data.map(repository => repository.full_name);
-    const repositories = Async.mapLimit(paths, 100, async path => getRepository(octokit, path));
-
-    return repositories;
-}
-
-function summarizeResults(response) {
-    const summarizations = response.reduce(
-        (previous, { data: { names: names } }) => {
-            names.push('git')
-
-            names.forEach(el => {
-                if (el in previous) {
-                    previous[el] += 1;
-                } else {
-                    previous[el] = 1;
-                }
-            });
-
-            return previous;
-        }, {});
-
-    console.log(summarizations);
-    return summarizations;
-}
-
-function fillRepositoryCount(original, data) {
-    original.sections.forEach(section => {
-        const topics = section.elements;
-
-        Object.keys(topics).forEach(key => {
-            const topic = topics[key].topic;
-
-            if (topic && topic in data) {
-                topics[key].message = `${data[key]}%20Repos`;
-            }
-        })
-    });
-
-    return original;
-}
 
 async function loadFile(filePath) {
     const content = await FileSystem.readFile(filePath);
@@ -78,7 +13,7 @@ async function loadFile(filePath) {
 }
 
 async function writeFile(filePath, data) {
-    return FileSystem.writeFile(filePath, data)
+    return FileSystem.writeFile(filePath, data);
 }
 
 function generateMarkdown(data, template) {
@@ -87,20 +22,101 @@ function generateMarkdown(data, template) {
     return markdown;
 }
 
+async function getRepositoryList(octokit) {
+    return octokit.paginate(
+        octokit.rest.repos.listForAuthenticatedUser,
+        {
+            visibility: 'all',
+            per_page: 100,
+        },
+    );
+}
+
+async function getRepositoryTopics(octokit, path) {
+    const [owner, repo] = path.split('/');
+
+    const response = octokit.rest.repos.getAllTopics({
+        owner,
+        repo,
+        // preview feature
+        mediaType: {
+            previews: [
+                'mercy',
+            ],
+        },
+    });
+
+    return response;
+}
+
+function summarizeResults(repositories) {
+    const topics = {};
+
+    // iterate over repositories topics
+    repositories.forEach(({ data: { names } }) => {
+        // add git topic by default ;)
+        names.push('git');
+
+        // add topics to the list
+        names.forEach((name) => {
+            if (name in topics) {
+                topics[name] += 1;
+            } else {
+                topics[name] = 1;
+            }
+        });
+    });
+
+    return topics;
+}
+
+function fillRepositoriesPerTopic(original, data) {
+    original.sections.forEach((section) => {
+        const topics = section.elements;
+
+        Object.keys(topics).forEach((key) => {
+            const { topic } = topics[key];
+
+            if (topic && topic in data) {
+                topics[key].message = `${data[key]}%20Repos`;
+            }
+        });
+    });
+
+    return original;
+}
+
 async function main() {
     // load environment
     DotEnv.config();
 
+    // Create the octokit instances to interact with github
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-    // TODO: iterate over pages
-    const response = getRepositories(octokit);
-    const template = loadFile('./templates/readme.md.handlebars')
+    const repositories = await getRepositoryList(octokit);
 
-    const summarized = fillRepositoryCount(bagdes, summarizeResults(await response));
-    const markdown = generateMarkdown(summarized, await template);
+    // get the full names only
+    const paths = repositories.map((repository) => repository.full_name);
+
+    // get all the topic for each repo
+    const fullRepositories = await Async.mapLimit(
+        paths, // the collection to iterate over
+        100, // max async ops at a time
+        async (path) => getRepositoryTopics(octokit, path),
+    );
+
+    const summarizedTopics = summarizeResults(fullRepositories);
+
+    // eslint-disable-next-line no-console
+    console.log(summarizedTopics);
+
+    const summarized = fillRepositoriesPerTopic(bagdes, summarizedTopics);
+
+    // load the template and write data
+    const template = await loadFile('./templates/readme.md.handlebars');
+    const markdown = generateMarkdown(summarized, template);
 
     await writeFile('readme.md', markdown);
 }
 
-main()
+main();
