098c1f84a204cf3e77dcbe001d38a01f7f6ab3b7
Uselessscat <arie.cbpro@gmail.com>
2024-03-30 02:38:01 -0300

    Update badge generation

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const FileSystem = require('fs').promises;
-const DotEnv = require('dotenv');
 const Async = require('async');
+const Filesystem = require('fs').promises;
+
+const DotEnv = require('dotenv');
 const Handlebars = require('handlebars');
-const { Octokit } = require('octokit');
+const { Octokit } = require('@octokit/rest');
+const makeBadge = require('badge-maker/lib/make-badge');
 
-const bagdes = require('./data/bagdes.json');
-
-async function loadFile(filePath) {
-    const content = await FileSystem.readFile(filePath);
-
-    return content.toString();
-}
-
-async function writeFile(filePath, data) {
-    return FileSystem.writeFile(filePath, data);
-}
+const badges = require('./data/badges.json');
 
 function generateMarkdown(data, template) {
+    console.log(data);
     const markdown = Handlebars.compile(template)(data);
 
     return markdown;
@@ -38,12 +31,6 @@ async function getRepositoryTopics(octokit, path) {
     const response = octokit.rest.repos.getAllTopics({
         owner,
         repo,
-        // preview feature
-        mediaType: {
-            previews: [
-                'mercy',
-            ],
-        },
     });
 
     return response;
@@ -70,23 +57,28 @@ function summarizeResults(repositories) {
     return topics;
 }
 
-function fillRepositoriesPerTopic(original, data) {
-    original.sections.forEach((section) => {
-        const topics = section.elements;
+async function generateRepositoryResults() {
+    const cacheFilePath = './cache.json'
+    const cacheExpiration = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
-        Object.keys(topics).forEach((key) => {
-            const { topic } = topics[key];
+    try {
+        const cacheStats = await Filesystem.stat(cacheFilePath);
 
-            if (topic && topic in data) {
-                topics[key].message = `${data[key]}%20Repos`;
-            }
-        });
-    });
+        if (Date.now() - cacheStats.mtimeMs < cacheExpiration) {
+            // Cache is still valid, read from cache file
+            const cacheData = await Filesystem.readFile(cacheFilePath);
 
-    return original;
-}
+            // eslint-disable-next-line no-console
+            console.log('Cache file exists and is still valid, reading from cache');
 
-async function main() {
+            return JSON.parse(cacheData);
+        }
+    } catch (error) {
+        // Cache file does not exist or error occurred, continue with generating results
+        // eslint-disable-next-line no-console
+        console.log(error, 'Cache file does not exist or expired, generating new results');
+    }
+
     // load environment
     DotEnv.config();
 
@@ -101,22 +93,93 @@ async function main() {
     // get all the topic for each repo
     const fullRepositories = await Async.mapLimit(
         paths, // the collection to iterate over
-        100, // max async ops at a time
+        100, // max async ops at a time, secondary gh rate limit
         async (path) => getRepositoryTopics(octokit, path),
     );
 
     const summarizedTopics = summarizeResults(fullRepositories);
 
+    // Write results to cache file
+    await Filesystem.writeFile(cacheFilePath, JSON.stringify(summarizedTopics));
+
+    return summarizedTopics;
+}
+
+function fillRepositoriesPerTopic(badges, data) {
+    for (const section of Object.keys(badges)) {
+        const topics = badges[section].elements;
+
+        for (const key of Object.keys(topics)) {
+            const { topic } = topics[key];
+
+            if (topic && topic in data) {
+                topics[key].message = `${data[key]} Repos`;
+            }
+        }
+    }
+}
+
+async function generateTemplateContents(badges) {
+    let contents = {};
+
+    for (const section of Object.keys(badges)) {
+        const elements = badges[section].elements;
+
+        // generate section badge
+        const filename = `./assets/badges/${section}.svg`;
+
+        contents[section] = {
+            label: badges[section].message,
+            badge: filename,
+            elements: {},
+        };
+
+        const badge = makeBadge({
+            label: '',
+            message: badges[section].message,
+            color: badges[section].color,
+        });
+        await Filesystem.writeFile(filename, badge);
+
+        // generate elements badges
+        for (const key of Object.keys(elements)) {
+            const filename = `./assets/badges/${section}_${key}.svg`;
+
+            const badge = makeBadge({
+                color: elements[key].color,
+                label: elements[key].label,
+                labelColor: elements[key].labelColor,
+                logo: elements[key].logo,
+                logoColor: elements[key].logoColor,
+                message: elements[key].message,
+            });
+            await Filesystem.writeFile(filename, badge);
+
+            contents[section].elements[key] = {
+                badge: filename,
+                label: elements[key].label,
+                url: elements[key].url,
+            };
+        }
+    }
+
+    return contents;
+}
+
+async function main() {
+    const summarizedTopics = await generateRepositoryResults();
+
     // eslint-disable-next-line no-console
     console.log(summarizedTopics);
 
-    const summarized = fillRepositoriesPerTopic(bagdes, summarizedTopics);
+    fillRepositoriesPerTopic(badges, summarizedTopics);
+    const templateContents = await generateTemplateContents(badges);
 
     // load the template and write data
-    const template = await loadFile('./templates/readme.md.handlebars');
-    const markdown = generateMarkdown(summarized, template);
+    const template = (await Filesystem.readFile('./templates/readme.md.handlebars')).toString();
+    const markdown = generateMarkdown(templateContents, template);
 
-    await writeFile('readme.md', markdown);
+    await Filesystem.writeFile('readme.md', markdown);
 }
 
 main();
